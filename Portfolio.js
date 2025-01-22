dae652b9993dcdc109f1b78c2f8c044ea3b28314
Anthony Tustin <ant.tustin@live.co.uk>
2025-01-22 21:18:31 +0000

    Adding weather and location arrays to create a full sentence as an output

--- Portfolio.js
+++ Portfolio.js
@@ -6,6 +6,10 @@ const arrayAdjective = ['vibrant', 'mysterious', 'serene', 'gritty', 'eccentric'
 
 const arrayNoun = ['Fox', 'Ox', 'Pig', 'Cat', 'Pony'];
 
+const arrayWeather = ['sunny', 'rainy', 'cloudy', 'snowy', 'windy'];
+
+const arrayLocation = ['London', 'Paris', 'Munich', 'New York', 'Washington'];
+
 function randomIndex(array) {
     return Math.floor(Math.random() * array.length);
 }
@@ -14,17 +18,21 @@ function indexArrays() {
     let verbIndex = randomIndex(arrayVerb);
     let adjectiveIndex = randomIndex(arrayAdjective);
     let nounIndex = randomIndex(arrayNoun);
-    
+    let locationIndex = randomIndex(arrayLocation);
+    let weatherIndex = randomIndex(arrayWeather);
+        
     return {
         Verb: arrayVerb[verbIndex],
         Adjective: arrayAdjective[adjectiveIndex],
         Noun: arrayNoun[nounIndex],
+        Weather: arrayWeather[weatherIndex],
+        Location: arrayLocation[locationIndex],
     };
 };
 
 let words = indexArrays();
 
-let sentence = words.Verb + " " + words.Adjective + " " + words.Noun + ".";
+let sentence = "The " + words.Adjective + " " + words.Verb + " " + words.Noun + " " + "went travelling to " + words.Weather + " " + words.Location + ".";
 
 console.log(sentence);
 
